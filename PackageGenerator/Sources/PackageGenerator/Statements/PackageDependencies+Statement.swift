5676d517071186b10d858bab17bccd5ae104e9b3
Teamcity Account <devnull@avito.ru>
2020-10-20 21:47:34 +0300

    Merge pull request #16991 in MA/avito-ios from MBS-9680_make_dependencies_idempotent to develop
    Automatically merge pull request #16991 in MA/avito-ios from MBS-9680_make_dependencies_idempotent to develop
    
    * commit '212335517fde3992e0b04af3447becd0f8b3acdd':
      MBS-9680: make package dependencies idempotent
    
    GitOrigin-RevId: 718d3d107a5a80f19f9a192146717fb026c6c420

--- PackageGenerator/Sources/PackageGenerator/Statements/PackageDependencies+Statement.swift
+++ PackageGenerator/Sources/PackageGenerator/Statements/PackageDependencies+Statement.swift
@@ -2,7 +2,10 @@ import Foundation
 
 public extension PackageDependencies {
     var statements: [String] {
-        external.map { (name: String, value: ExternalPackageLocation) -> String in
+        external.sorted { left, right -> Bool in
+            left.key < right.key
+        }
+        .map { (name: String, value: ExternalPackageLocation) -> String in
             switch value {
             case let .url(url, version, _):
                 return ".package(url: \"\(url)\", \(version.statement))"
