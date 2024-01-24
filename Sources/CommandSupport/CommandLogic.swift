3f9cae463a759b239991509465b3d409d5069746
Aleksandr Goncharov <asgoncharov@avito.ru>
2024-01-24 11:30:43 +0300

    Pull request #32839: MBS-14707: ai cleanup and refactoring
    Merge in MA/avito-ios from MBS-14707-rerun-pods to develop
    
    * commit '782280b9df570828283c8ba5390055f28c3714ab':
      MBS-14707: PR fixes
      MBS-14707: Fix tests for xcode 14
      MBS-14707: Tune indep logs
      MBS-14707: Build ai binary
      MBS-14707: Refactor indep project installer
      MBS-14707: Remove dead code
      MBS-14707: Tune git lfs installation
      MBS-14707: Add project selector dialog
      MBS-14707: Remove dispatch group from async context
      MBS-14707: Fix lint issues
      MBS-14707: Replace stdinreader with console
      MBS-14707: Remove calls to print function
      MBS-14707: Remove strings constants from logger init
      MBS-14707: Use console from the di
      MBS-14707: Remove deprecated commands
      MBS-14707: Remove outdated health check hint
      MBS-14707: Remove old survey banner
    
    GitOrigin-RevId: 88b975c35bf4099df8f6a8f57dcaa863cf2b0dee

--- Sources/CommandSupport/CommandLogic.swift
+++ Sources/CommandSupport/CommandLogic.swift
@@ -1,3 +1,21 @@
 public protocol CommandLogic {
     func run() throws
 }
+
+extension CommandLogic {
+    public func toAsync() -> AsyncCommandLogic {
+        SyncCommandLogicWrapper(command: self)
+    }
+}
+
+private final class SyncCommandLogicWrapper: AsyncCommandLogic {
+    private let job: () throws -> ()
+    
+    init(command: CommandLogic) {
+        job = command.run
+    }
+    
+    func run() async throws {
+        try job()
+    }
+}
