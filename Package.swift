0c0f38f2ae599afa4639dcc663c3e1515d91bb48
Georgiy Kiriy <gnikiriy@avito.ru>
2026-01-15 23:41:49 +0300

    Pull request #46017: MA-5995 Миграция на NIO-based MutualTLS модуль и обновление владельцев кода
    Merge in MA/avito-ios from MA-5995-update-code-owners to develop
    
    * commit 'd3135d9c37be71c128946b46c040f5e34020300a':
      MA-5995: Add package mirrors
      MA-5995: Update binaries
      MA-5995: Update owners
      MA-5995: Add nio-based mtls module
      MA-5995: Fix bitbucket ownership generation
    
    GitOrigin-RevId: 1019556e302a93cbb4b0fcf80d2d91261833513b

--- Package.swift
+++ Package.swift
@@ -494,19 +494,6 @@ targets.append(
         path: "Sources/MetricsUtils"
     )
 )
-// MARK: MutualTLS
-targets.append(
-    .target(
-        name: "MutualTLS",
-        dependencies: [
-            "DI",
-            "Environment",
-            "PathLib",
-            "ProcessController",
-        ],
-        path: "Sources/MutualTLS"
-    )
-)
 // MARK: PathLib
 targets.append(
     .target(
@@ -933,7 +920,6 @@ let package = Package(
         .library(name: "MetricsRecording", targets: ["MetricsRecording"]),
         .library(name: "MetricsTestHelpers", targets: ["MetricsTestHelpers"]),
         .library(name: "MetricsUtils", targets: ["MetricsUtils"]),
-        .library(name: "MutualTLS", targets: ["MutualTLS"]),
         .library(name: "PathLib", targets: ["PathLib"]),
         .library(name: "PlistLib", targets: ["PlistLib"]),
         .library(name: "ProcessController", targets: ["ProcessController"]),
